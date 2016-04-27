41c91520dd58c308becaf7b3e44afd8221c8e803
sxu <sxu@10.101.1.120>
2016-04-27 16:45:03 -0700

    added deploy script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,10 @@
 echo "deploying Pure Xml Spring Application..."
 
 #remove the old folder and application war file from tomcat
-rm -rf /opt/apache-tomcat/webapps/PureXmlSpring-1.0-SNAPSHOT.war
-rm -rf /opt/apache-tomcat/webapps/PureXmlSpring-1.0-SNAPSHOT
+rm -rf /opt/apache-tomcat/webapps/PureXmlSpring.war
+rm -rf /opt/apache-tomcat/webapps/PureXmlSpring
 
-cp ../dist/PureXmlSpring-1.0-SNAPSHOT.war   /opt/apache-tomcat/webapps
+cp ../dist/PureXmlSpring.war   /opt/apache-tomcat/webapps
 
 
 
