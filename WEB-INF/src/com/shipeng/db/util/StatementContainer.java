bd45d0525ecd852f1abfd9423ed4493bd660e277
sxu <sxu@10.101.1.201>
2016-05-06 14:31:49 -0700

    added StatementContainer class for sql container.

---
+++ WEB-INF/src/com/shipeng/db/util/StatementContainer.java
@@ -0,0 +1,22 @@
+package com.shipeng.db.util;
+
+public class StatementContainer {
+	
+	private final String statement;
+
+	  /**
+	   * @return the statement
+	   */
+	  public final String getStatement() {
+	    return statement;
+	  }
+
+	  /**
+	   * @param statement
+	   */
+	  public StatementContainer(String statement) {
+	    this.statement = statement;
+	  }
+}//end class
+
+
